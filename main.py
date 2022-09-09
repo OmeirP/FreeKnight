46af580a7f0e18a4dbcab09f5c45e19c33742de3
Omeir <50913215+OmeirP@users.noreply.github.com>
2022-09-09 22:21:35 +0100

    loaded sprite images and swapped pygame setup and object sections

--- main.py
+++ main.py
@@ -11,15 +11,39 @@ import os
 #display_height = 900
 
 
-red = (255,0,0)
-blue = (0,255,0)
-green = (0,0,255)
+red = (255, 0, 0)
+blue = (0, 255, 0)
+green = (0, 0, 255)
 
 
 fps=60
 
 
 gaming=True
+
+
+
+
+
+############ objects
+
+class playerClass:
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.imgsList = []
+        for i in range(1,11):
+            
+            img = pygame.image.load(os.path.join("FreeKnight_v1/Colour1/NoOutline/SeparatePngs/run", "run" + str(i) + ".png")).convert()            
+            self.imgsList.append(img)
+            
+            self.firstImg = self.images[0]  # just to get a picture from the cycle with the same dimensions as the rest to use to get rect
+            
+            self.rect = self.firstImg.get_rect()
+
+
+
 
 
 ############## pygame setup things
@@ -29,18 +53,19 @@ clock=pygame.time.Clock()
 
 pygame.init()
 
+infoObject = pygame.display.Info()
 
-gameDisplay=pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+gameDisplay=pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-background = pygame.image.load(os.path.join("Legacy-Fantasy-VL.1 - High Forest - Update 1.5","background.png"))
+background = pygame.image.load(os.path.join("Legacy-Fantasy-VL.1 - High Forest - Update 1.5/background","background.png")).convert()
+
+background = pygame.transform.scale(background, (infoObject.current_w, infoObject.current_h))
 
 gameDisplayRect=gameDisplay.get_rect()
 
-print(gameDisplayRect)
 
 
 
-############ classes, funcs
 
 
 
@@ -73,6 +98,13 @@ while gaming:
                     sys.exit()
                 finally:
                     gaming=False
+    
+    
+    
+    gameDisplay.blit(background, gameDisplayRect)
+    
+    pygame.display.update()
+    clock.tick(fps)
                 
 
 
