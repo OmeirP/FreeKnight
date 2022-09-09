f7fe83cdbf784f60cfbd7e065eaec82ff7248928
Omeir <50913215+OmeirP@users.noreply.github.com>
2022-09-09 21:34:59 +0100

    extracted level images zip file and did more pygame setup

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ import sys
 import os
 
 
-# all variables will go here
+########## all variables will go here
 
-display_width = 1600
-display_height = 900
+
+
+#display_width = 1600
+#display_height = 900
+
+
+red = (255,0,0)
+blue = (0,255,0)
+green = (0,0,255)
 
 
 fps=60
 
 
-#pygame setup things
+gaming=True
+
+
+############## pygame setup things
 
 
 clock=pygame.time.Clock()
@@ -20,18 +30,50 @@ clock=pygame.time.Clock()
 pygame.init()
 
 
+gameDisplay=pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+
+background = pygame.image.load(os.path.join("Legacy-Fantasy-VL.1 - High Forest - Update 1.5","background.png"))
+
+gameDisplayRect=gameDisplay.get_rect()
+
+print(gameDisplayRect)
 
 
 
-# classes, funcs
+############ classes, funcs
 
 
 
-# first run things here
+############ first run things here
 
 
 
 
 
 
-#game loop
+###########game loop
+
+while gaming:
+    
+    
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            try:
+                sys.exit()
+            finally:
+                gaming=False
+                
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                try:
+                    sys.exit()
+                finally:
+                    gaming=False
+                
+
+
+
