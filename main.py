dcc64ab7370e3338956acc08b0eebf960a2cbe80
Omeir <50913215+OmeirP@users.noreply.github.com>
2022-09-09 20:29:36 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import pygame
+import sys
+import os
+
+
+# all variables will go here
+
+display_width = 1600
+display_height = 900
+
+
+fps=60
+
+
+#pygame setup things
+
+
+clock=pygame.time.Clock()
+
+pygame.init()
+
+
+
+
+
+# classes, funcs
+
+
+
+# first run things here
+
+
+
+
+
+
+#game loop
